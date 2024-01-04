7db049ad58864f72e61c1f48bc7cc16bdfc34e87
yang_zhuo <2431542525@qq.com>
2024-01-04 16:34:36 +0800

    feat(Hufman tree)：Huffman node structure and function  declaration

--- Huffman_tree_encoder_and_decoder_simulation/huftree_generate.h
+++ Huffman_tree_encoder_and_decoder_simulation/huftree_generate.h
@@ -1,4 +1,20 @@
 #ifndef DATA_STRUCTURES_AND_ALGORITHMS_HUFTREE_GENERATE_H
 #define DATA_STRUCTURES_AND_ALGORITHMS_HUFTREE_GENERATE_H
 
+#include<stdio.h>
+#include<stdlib.h>
+
+// Huffman tree node structure definition
+typedef struct HuffmanNode{
+    char character;
+    char frequency;
+    struct HuffmanNode* left;
+    struct HuffmanNode* right;
+}HuffmanNode;
+
+// Function declaration
+HuffmanNode* generateHuffmanTree(int n);
+void printHuffmanTree(HuffmanNode* root,int depth);
+void encodeCharacters(HuffmanNode* root,char* code,int top);
+
 #endif //DATA_STRUCTURES_AND_ALGORITHMS_HUFTREE_GENERATE_H
